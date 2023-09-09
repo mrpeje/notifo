93794f9f8edb9bca067929eee1e5adfebe589a2e
MRPEJE <whond.earn.war@gmail.com>
2023-09-09 17:34:15 +0400

    Intel-Tele gateway

---
+++ backend/src/Notifo.Domain.Integrations/SMSGyteways/Gyteways/Intel-tele/IntelTeleGateway.cs
@@ -0,0 +1,62 @@
+﻿using Notifo.Domain.Integrations.SMSGyteways.Gyteways.Intel_tele;
+using Notifo.Domain.Integrations.SMSGyteways.Interfaces;
+
+namespace Notifo.Domain.Integrations.SMSGyteway;
+public class IntelTeleGateway : ISMSGateway
+{
+    private readonly IHttpClientFactory httpClientFactory;
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string From { get; set; }
+
+    public IntelTeleGateway(IHttpClientFactory httpClientFactory)
+    {
+        this.httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<DeliveryResult> SendSMSAsync(SmsMessage message)
+    {
+        using (var httpClient = httpClientFactory.CreateClient("IntelTeleGateway"))
+        {
+            httpClient.BaseAddress = new Uri("http://api.sms.intel-tele.com/");
+            var url = "message/send/?";
+            var parameters = new System.Collections.Specialized.NameValueCollection
+            {
+                { "username", Login },
+                { "api_key", Password },
+                { "from", From },
+                { "to", message.To },
+                { "message", message.Text },
+            };
+
+            var urlWithParams = url + ToQueryString(parameters);
+
+            var response = await httpClient.GetAsync(urlWithParams);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var responseBody = Newtonsoft.Json.JsonConvert.DeserializeObject<ReplyModel>(responseContent);
+            if (responseBody != null && responseBody.Reply.Count > 0)
+            {
+                // Process the first response, since there is only 1 number in the request
+                var reply = responseBody.Reply.FirstOrDefault();
+
+                if (reply != null && reply.Status.Contains("OK", StringComparison.Ordinal))
+                {
+                    return DeliveryResult.Sent;
+                }
+
+                if (reply != null && reply.Status.Contains("error", StringComparison.Ordinal))
+                {
+                    return DeliveryResult.Failed(responseContent);
+                }
+            }
+
+            return DeliveryResult.Failed("Empty response");
+        }
+    }
+
+    private static string ToQueryString(System.Collections.Specialized.NameValueCollection nvc)
+    {
+        return string.Join("&", Array.ConvertAll(nvc.AllKeys, key => $"{key}={nvc[key]}"));
+    }
+}
---
+++ backend/src/Notifo.Domain.Integrations/SMSGyteways/Gyteways/Intel-tele/ReplyModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Notifo.Domain.Integrations.SMSGyteways.Gyteways.Intel_tele;
+public class ReplyModel
+{
+    [JsonProperty("reply")]
+    public List<Response> Reply { get; set; }
+}
+public class Response
+{
+    [JsonProperty("status")]
+    public string Status { get; set; }
+
+    [JsonProperty("parts")]
+    public int Parts { get; set; }
+
+    [JsonProperty("cost")]
+    public double Cost { get; set; }
+
+    [JsonProperty("number")]
+    public string Number { get; set; }
+
+    [JsonProperty("message_id")]
+    public string MessageId { get; set; }
+
+    [JsonProperty("country")]
+    public string Country { get; set; }
+
+    [JsonProperty("mccmnc")]
+    public string MccMnc { get; set; }
+}
